41e64756265edc7d368a4a7361900d127c983d54
Mathias Schilling <m@matchilling.com>
2018-07-13 01:00:31 +0200

    Updates

--- path---talk-eve-by-chris-granger-3e26ef15f9f61268f5e5.js
+++ path---talk-eve-by-chris-granger-3e26ef15f9f61268f5e5.js
@@ -1,2 +1,2 @@
-webpackJsonp([99278232501819],{2956:function(e,n){e.exports={data:{talkJson:{description:null,meta:{duration:"PT46M4S"},presenter:{name:null},slug:"eve-by-chris-granger",source:{id:"5V1ynVyud4M",provider:"youtube"},tag:[],title:'"Eve" by Chris Granger'},allTalkJson:null},pathContext:{slug:"eve-by-chris-granger",tags:[]}}}});
+webpackJsonp([99278232501819],{2963:function(e,n){e.exports={data:{talkJson:{description:null,meta:{duration:"PT46M4S"},presenter:{name:null},slug:"eve-by-chris-granger",source:{id:"5V1ynVyud4M",provider:"youtube"},tag:[],title:'"Eve" by Chris Granger'},allTalkJson:null},pathContext:{slug:"eve-by-chris-granger",tags:[]}}}});
 //# sourceMappingURL=path---talk-eve-by-chris-granger-3e26ef15f9f61268f5e5.js.map
