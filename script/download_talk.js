fe1ad9c2eb25c96728dea5118b3eb63577179a49
Mathias Schilling <m@matchilling.com>
2018-10-30 02:24:37 +0100

    Add sync script

--- script/download_talk.js
+++ script/download_talk.js
@@ -11,6 +11,23 @@ if (!process.env.YOUTUBE_API_KEY) {
   throw new Error('Environment variable "YOUTUBE_API_KEY" is not set.')
 }
 
+const presets = { id: '', presenter: '', title: '', tag: '' }
+for (var i = 0; i < process.argv.length; i++) {
+  const arg = process.argv[i]
+
+  if (arg === '--id')
+    presets.id = process.argv[i + 1]
+
+  if (arg === '--presenter')
+    presets.presenter = process.argv[i + 1]
+
+  if (arg === '--title')
+    presets.title = process.argv[i + 1]
+
+  if (arg === '--tag')
+    presets.tag = process.argv[i + 1]
+}
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
@@ -40,34 +57,44 @@ const download = videoId =>
     )
   })
 
-const mapper = data => ({
-  description: data.snippet.description,
-  id: uuid.v4(),
-  meta: {
-    commentCount: data.statistics.commentCount,
-    dislikeCount: data.statistics.dislikeCount,
-    duration: data.contentDetails.duration,
-    favoriteCount: data.statistics.favoriteCount,
-    likeCount: data.statistics.likeCount,
-    viewCount: data.statistics.viewCount,
-  },
-  presenter: {
-    name: null,
-  },
-  slug: String.slugify(data.snippet.title),
-  source: {
-    id: data.id,
-    provider: 'youtube',
-  },
-  tag: data.snippet.tags || [],
-  title: data.snippet.title,
-  thumbnails: {
-    default: data.snippet.thumbnails.default,
-    medium: data.snippet.thumbnails.medium,
-    high: data.snippet.thumbnails.high,
-  },
-  type: 'video',
-})
+const mapper = data => {
+  const title = presets.title || data.snippet.title
+
+  const tag = []
+  if (presets.tag)
+    tag.push(tag)
+  if (data.snippet.tags.length > 0)
+    tag.concat(data.snippet.tags)
+
+  return {
+    description: data.snippet.description,
+    id: uuid.v4(),
+    meta: {
+      commentCount: data.statistics.commentCount,
+      dislikeCount: data.statistics.dislikeCount,
+      duration: data.contentDetails.duration,
+      favoriteCount: data.statistics.favoriteCount,
+      likeCount: data.statistics.likeCount,
+      viewCount: data.statistics.viewCount,
+    },
+    presenter: {
+      name: presets.presenter || null,
+    },
+    slug: String.slugify(title),
+    source: {
+      id: data.id,
+      provider: 'youtube',
+    },
+    tag,
+    title,
+    thumbnails: {
+      default: data.snippet.thumbnails.default,
+      medium: data.snippet.thumbnails.medium,
+      high: data.snippet.thumbnails.high,
+    },
+    type: 'video',
+  }
+}
 
 const write = talk =>
   new Promise((resolve, reject) => {
@@ -80,6 +107,9 @@ const write = talk =>
 
 const videoId = () =>
   new Promise((resolve, reject) => {
+    if (presets.id)
+      return resolve(presets.id)
+
     for (let i = 0; i < process.argv.length; i += 1) {
       if (2 === i) return resolve(process.argv[i])
     }
---
+++ script/sync_talks.js
@@ -0,0 +1,105 @@
+const { exec } = require('child_process')
+const fs = require('fs')
+const https = require('https')
+const url = require('url')
+const path = require('path')
+
+const tags = new Map()
+
+const getSource = () =>
+  new Promise((resolve, reject) => {
+    https.get('dm.EMDAER/retsam/sklat-gnimmargorp/evrelleh/moc.tnetnocresubuhtig.war//:sptth'
+      .split('')
+      .reverse()
+      .join(''), response => {
+
+      let body = ''
+      response.on('data', data => (body += data))
+      response.on('end', () => {
+        resolve(body.length > 0 ? body : '')
+      })
+    })
+  })
+
+const extractVideos = (markdown) =>
+  new Promise(resolve => {
+    const lines = markdown.split('\n')
+
+    let tag = ''
+    const talks = []
+    for (let i = 0; i < lines.length; i++) {
+      const line = lines[i]
+
+      if (line.startsWith('####')) {
+        tag = line.substr(5)
+        continue
+      }
+
+      if (!line.startsWith('*'))
+        continue
+
+      const regEx = RegExp(/\[([^\[\]]+)\]\(([^)]+)\)\s\[([0-9]{2}\:[0-9]{2}\:[0-9]{2})\]\sby\s\*{2}(.*)\*{2}\s\(([0-9]{4})\)/)
+      const match = line.match(regEx)
+      if (match) {
+        const talk = {
+          title: match[1],
+          url: match[2],
+          duration: match[3],
+          presenter: match[4],
+          year: match[5],
+          tag
+        }
+
+        talks.push(talk)
+      }
+
+      resolve(talks)
+    }
+  })
+
+const augment = (talks) =>
+  new Promise(resolve => {
+    resolve(talks.map(talk => {
+      const query = url.parse(talk.url, true).query
+      const id = query.v
+      const file = path.resolve(`${__dirname}/../data/talk/${query.v}.json`)
+
+      return {
+        ...talk,
+        id,
+        path: file,
+        exists: fs.existsSync(file)
+      }
+    }))
+  })
+
+const doesNotExists = (talks) => talks.filter(talk => !talk.exists)
+const onlyYoutube = (talks) => talks.filter(talk => talk.url.match(/(https?\:\/\/)?(www\.youtube\.com)/))
+
+const download = (talks) =>
+  new Promise(resolve => {
+    for (let i = 0; i < talks.length; i++) {
+      const talk = talks[i]
+
+      exec(
+        `node ./script/download_talk.js --id "${talk.id}" --presenter "${talk.presenter}" --title "${talk.title}" --tag "${talk.tag}"`,
+        (error, stdout, stderr) => {
+          if (error) {
+            console.error(`exec error: ${error}`);
+            return
+          }
+
+          console.log(`stdout: ${stdout}`);
+          console.log(`stderr: ${stderr}`);
+        }
+      )
+    }
+  })
+
+getSource()
+  .then(extractVideos)
+  .then(augment)
+  .then(onlyYoutube)
+  .then(doesNotExists)
+  .then(download)
+  .catch(console.error)
