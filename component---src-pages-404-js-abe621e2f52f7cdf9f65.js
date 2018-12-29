7086431c9cc7cc95978e26abaa031e7a209a41a3
Mathias Schilling <m@matchilling.com>
2018-12-29 21:39:59 +0100

    Updates

---
+++ component---src-pages-404-js-abe621e2f52f7cdf9f65.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{140:function(e,t,n){"use strict";n.r(t);var a=n(0),u=n.n(a);t.default=function(){return u.a.createElement("div",null,u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-abe621e2f52f7cdf9f65.js.map
