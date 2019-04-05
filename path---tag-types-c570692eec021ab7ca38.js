2fdaa2ce96aae974e081da36b455b857b4b7f031
Mathias Schilling <m@matchilling.com>
2019-04-05 11:08:08 +0200

    Updates

--- path---tag-types-c570692eec021ab7ca38.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([2856434073045],{2805:function(e,t){e.exports={data:{allTalkJson:{edges:[{node:{description:" ",meta:{duration:"PT1H4M35S"},presenter:{name:null},source:{id:"YScIPA8RbVE",provider:"youtube"},slug:"the-algebra-of-algebraic-data-types",tag:["London","Haskell","Algebra","Algebraic","Types"],thumbnails:{default:{url:"https://i.ytimg.com/vi/YScIPA8RbVE/default.jpg",height:90,width:120}},title:"The Algebra of Algebraic Data Types"}}]}},pathContext:{slug:"types",tag:"Types"}}}});
-//# sourceMappingURL=path---tag-types-c570692eec021ab7ca38.js.map
